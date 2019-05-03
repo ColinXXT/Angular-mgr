eb7cf7136e920e96884a847ec75f59cf83c22239
jiessie <jiessie@jiessiedeMacBook-Pro.local>
2019-05-03 15:42:27 +0800

    Sumit svg meterial module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,29 @@
-import { NgModule , SkipSelf, Optional} from '@angular/core';
+import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { LoadSvgResources } from '../utils/svg.util';
 /**
  * 适合只加载一次的组建或者服务
  *  */
 
 @NgModule({
   declarations: [
-    HeaderComponent, 
-    FooterComponent, 
+    HeaderComponent,
+    FooterComponent,
     SidebarComponent
   ],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   exports: [
     HeaderComponent,
@@ -27,9 +34,14 @@ import { MatToolbarModule } from '@angular/material';
 export class CoreModule {
   //SkipSelf 去系统父级资源池查找
   //第一次加载，系统没有coreModule 
-  constructor(@Optional() @SkipSelf() parent: CoreModule){
-    if(parent){
+  constructor(@Optional() @SkipSelf()
+  parent: CoreModule,
+    ir: MatIconRegistry,
+    ds: DomSanitizer
+  ) {
+    if (parent) {
       throw new Error("模块已经存在，不能再次加载！")
     }
+    LoadSvgResources(ir, ds);
   }
- }
+}
